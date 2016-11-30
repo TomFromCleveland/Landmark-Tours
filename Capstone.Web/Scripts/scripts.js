9ed380aa1e50680305777e7bdb4f236515f6cc1a
Rami Rizk <ramirizk87@gmail.com>
2016-11-30 00:36:45 -0500

    made adding landmark searchable

--- Capstone.Web/Scripts/scripts.js
+++ Capstone.Web/Scripts/scripts.js
@@ -5,11 +5,7 @@
         locations.push(temp);
     }
 
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 10,
-        center: new google.maps.LatLng(41.500473, -81.693750),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
+    var map = newMap();
     var infowindow = new google.maps.InfoWindow();
 
     var marker, i;
@@ -27,4 +23,90 @@
             }
         })(marker, i));
     }
+
+
 };
+
+
+function addLandmark() {
+    var map = newMap();
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function () {
+        searchBox.setBounds(map.getBounds());
+    });
+
+    var markers = [];
+    searchBox.addListener('places_changed', function () {
+        places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+
+        // Clear out the old markers.
+        markers.forEach(function (marker) {
+            marker.setMap(null);
+        });
+
+        markers = [];
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function (place) {
+            if (!place.geometry) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+            var icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+                map: map,
+                icon: icon,
+                title: place.name,
+                animation: google.maps.Animation.DROP,
+                position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+
+        });
+        map.fitBounds(bounds);
+        return places;
+    });
+    
+}
+
+function newMap() {
+    return new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: new google.maps.LatLng(41.500473, -81.693750),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+}
+
+$(document).ready(function () {
+    $("#landmark_submit").click(function () {
+        $('#LandmarkName').val(places[0].name);
+        $('#StreetAddress').val(places[0].address_components[0].long_name + ' ' + places[0].address_components[1].short_name);
+        $('#City').val(places[0].address_components[2].long_name);
+        $('#State').val(places[0].address_components[4].long_name);
+        $('#zip').val(places[0].address_components[6].short_name);
+        $('.submission_form').submit();
+    });
+
+
+
+});
